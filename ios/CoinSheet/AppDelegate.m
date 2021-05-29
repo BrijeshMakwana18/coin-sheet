0981fce2d0e959b1d058279a88fef7c00cabe618
brijesh18012000 <brijesh18012000@gmail.com>
2021-05-29 19:00:47 +0530

    Libraries integrated

--- ios/CoinSheet/AppDelegate.m
+++ ios/CoinSheet/AppDelegate.m
@@ -3,7 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import <Firebase.h>
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -48,6 +48,9 @@ static void InitializeFlipper(UIApplication *application) {
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   [RNSplashScreen show];
+  if ([FIRApp defaultApp] == nil) {
+      [FIRApp configure];
+    }
   return YES;
 }
 
