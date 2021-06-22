88e1372679fcbb944355868db98cbdd2aef8943b
brijesh18012000 <brijesh18012000@gmail.com>
2021-06-22 22:46:23 +0530

    Animations added in Launch

--- src/screens/Launch/index.js
+++ src/screens/Launch/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import 
+    React,{
+    useRef,
+    useEffect
+} from 'react'
 import { 
     View, 
     Text, 
     StyleSheet, 
     StatusBar, 
-    Image 
+    Image,
+    Animated 
 } from 'react-native'
 import { 
     images, 
@@ -17,6 +22,43 @@ import {
     Button
 } from '../../components'
 export default function Launch({ navigation }) {
+    const slideAnimation = useRef(new Animated.Value(perfectSize(200))).current
+    const height = useRef(new Animated.Value(perfectSize(400))).current
+    const width = useRef(new Animated.Value(perfectSize(400))).current
+    const opacity = useRef(new Animated.Value(0)).current
+
+    const slideUp = () => {
+        Animated.parallel([
+            Animated.timing(slideAnimation, {
+                toValue: perfectSize(80),
+                duration: 2000,
+                useNativeDriver: false
+            }),
+            Animated.timing(height, {
+                toValue: perfectSize(200),
+                duration: 2000,
+                useNativeDriver: false
+            }),
+            Animated.timing(width, {
+                toValue: perfectSize(200),
+                duration: 2000,
+                useNativeDriver: false
+            }),
+        ]).start()
+        
+    }
+
+    useEffect(()=>{
+        slideUp()
+        setTimeout(()=> {
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: 2000,
+                useNativeDriver: false
+            }).start()
+        },2000)
+    },[])
+
     return (
         <View 
             style={styles.container}
@@ -26,43 +68,55 @@ export default function Launch({ navigation }) {
                 backgroundColor={colors.backgroundColor} 
                 barStyle='dark-content' />
 
-            <Image
-                source={images.launchScreenLogo} 
-                style={styles.images}
-                resizeMode='contain' />
-
-            <Text 
-                style={styles.title}
-            >
-                {strings.launchScreen.title}
-            </Text>
-
-            <Text 
-                style={styles.subTitle}
-            >
-                {strings.launchScreen.subTitle}
-            </Text>
-
-            <Image 
-                source={images.goal}
-                style={{height: '10%',width: '20%'}}
-                resizeMode='contain'
-            />
+            <Animated.View 
+                style={{ 
+                    height: height, 
+                    width: width, 
+                    marginTop: slideAnimation 
+                }}>
             
-            <View style={styles.bottomView}>
+                <Image
+                    source={images.launchScreenLogo} 
+                    style={styles.images}
+                    resizeMode='contain' />
 
-                <Button 
-                    buttonTitle={strings.launchScreen.signupTitle}
-                    onPress={()=>navigation.navigate('Signup')} />
+            </Animated.View>
+                
+            <Animated.View style={{flex: 1, opacity: opacity}} >
+                <Text 
+                    style={styles.title}
+                >
+                    {strings.launchScreen.title}
+                </Text>
 
                 <Text 
-                    onPress={()=>navigation.navigate('Login')} 
-                    style={styles.loginButton}
+                    style={styles.subTitle}
                 >
-                    {strings.launchScreen.loginTitle}
+                    {strings.launchScreen.subTitle}
                 </Text>
-           
-            </View>
+
+                <Image 
+                    source={images.goal}
+                    style={{height: perfectSize(50),width: perfectSize(50), alignSelf: 'center'}}
+                    resizeMode='contain'
+                />
+                
+                </Animated.View>
+                
+                <Animated.View style={[styles.bottomView, { opacity: opacity }]}>
+
+                    <Button 
+                        buttonTitle={strings.launchScreen.signupTitle}
+                        onPress={()=>navigation.navigate('Signup')} />
+
+                    <Text 
+                        onPress={()=>navigation.navigate('Login')} 
+                        style={styles.loginButton}
+                    >
+                        {strings.launchScreen.loginTitle}
+                    </Text>
+            
+                </Animated.View>
 
         </View>
     )
@@ -75,8 +129,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     images: {
-        marginTop: '10%',
-        height: '35%',
+        height: '100%',
         width: '100%',
     },
     title: {
@@ -96,8 +149,8 @@ const styles = StyleSheet.create({
     },
     bottomView: {
         position: 'absolute',
-        bottom: '5%',
-        alignSelf: 'center'
+        alignSelf: 'center',
+        bottom: '5%'
     },
     loginButton: {
         color: colors.textColor,
