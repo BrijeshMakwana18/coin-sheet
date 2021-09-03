619b2e42e15fc134ba0edea7fdd85b627d834fa3
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-09-03 19:00:48 +0530

    comments added to addTransaction function

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -39,49 +39,59 @@ class Home extends Component {
     this.state = {};
   }
 
+  //Fetching current user and storing it into store
   getCurrentUserData = async () => {
     let user = await auth().currentUser;
     this.props.setUserData(user);
+    //To fetch expenses and income from transactions collection
     await this.getUserTransactions();
   };
 
   getUserTransactions = async () => {
     const userId = this.props.appReducer.user.uid;
+    //Fetching all expenses
     this.userExpenses = await firestore()
       .collection('transactions')
       .doc(userId)
       .collection('expenses')
       .onSnapshot(async documentSnapshot => {
         let tempAllExpenses = [];
+        //Fetching each document data from expenses collenction
         documentSnapshot.forEach(async doc => {
           let tempExpense = doc.data();
           if (!tempAllExpenses.includes(tempExpense)) {
             tempAllExpenses.push(tempExpense);
           }
         });
+        //Sum of all expenses
         let totalExpense = this.getTransactionSum(tempAllExpenses);
+        //Storing values into store
         this.props.setUserExpenses(tempAllExpenses);
         this.props.setTotalExpenses(totalExpense);
       });
-
+    //Fetching all income
     this.userIncome = await firestore()
       .collection('transactions')
       .doc(userId)
       .collection('income')
       .onSnapshot(async documentSnapshot => {
         let tempAllIncome = [];
+        //Fetching each document data from income collenction
         documentSnapshot.forEach(doc => {
           let tempIncome = doc.data();
           if (!tempAllIncome.includes(tempIncome)) {
             tempAllIncome.push(tempIncome);
           }
         });
+        //Sum of all income
         let totalIncome = this.getTransactionSum(tempAllIncome);
+        //Storing income into store
         this.props.setUserIncome(tempAllIncome);
         this.props.setTotalIncome(totalIncome);
       });
   };
 
+  //To calculate sum of transactions
   getTransactionSum = transactions => {
     let sum = 0;
     for (let i = 0; i < transactions.length; i++) {
@@ -95,6 +105,7 @@ class Home extends Component {
   }
 
   componentWillUnmount() {
+    //Removing snapshot listener
     this.userExpenses();
     this.userIncome();
   }
